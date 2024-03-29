53a26beb6477382c898ff6dce31eedafb9dbf4c7
lenakravt <164635932+lenakravt@users.noreply.github.com>
2024-03-30 02:21:16 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+from tabulate import tabulate
 
-url = 'https://otzov-mf.ru/tinkoff-bank-otzyvy/'
+url = input()
 
 
 def load_data(url):
@@ -21,23 +22,10 @@ def load_data(url):
     return reviews_clean, users_clean
 
 
+#результат можно оформить в виде таблицы, в первом столбце будут находится имена пользователей, во втором - текст непосредственно самого отзыва и т.д.
 data = load_data(url)
 result = {}
 for user in data[1]:
     result[user] = data[0][data[0].index([user]) + 1][0]
 #print(result)
-
-from tabulate import tabulate
-#print(tabulate(result.items(), headers=['NAME', 'VALUE'], tablefmt="grid"))
-
-
-
-
-
-
-
-result_values = []
-for k in result.values():
-    result_values.append(k)
-#print(result_values)
-
+print(tabulate(result.items(), headers=['NAME', 'VALUE'], tablefmt="grid"))
