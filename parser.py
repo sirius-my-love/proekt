b72ad2fb37c74ec8f18f25f34b6ff8d64f7dbd5c
lenakravt <164635932+lenakravt@users.noreply.github.com>
2024-03-30 16:35:48 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-from tabulate import tabulate
+
 
 url = input()
 
@@ -22,10 +22,25 @@ def load_data(url):
     return reviews_clean, users_clean
 
 
-#результат можно оформить в виде таблицы, в первом столбце будут находится имена пользователей, во втором - текст непосредственно самого отзыва и т.д.
 data = load_data(url)
 result = {}
 for user in data[1]:
     result[user] = data[0][data[0].index([user]) + 1][0]
 #print(result)
-print(tabulate(result.items(), headers=['NAME', 'VALUE'], tablefmt="grid"))
+
+#результат можно оформить в виде таблицы, в первом столбце будут находится имена пользователей, во втором - текст непосредственно самого отзыва и т.д.
+
+import xlsxwriter
+
+workbook = xlsxwriter.Workbook('data.xlsx')
+worksheet = workbook.add_worksheet()
+res = result.items()
+row = 0
+col = 0
+
+for item, cost in (res):
+    worksheet.write(row, col,     item)
+    worksheet.write(row, col + 1, cost)
+    row += 1
+
+workbook.close()
